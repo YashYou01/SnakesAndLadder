41eb2fef01c35376a8b526e048ccd7f67430436d
YashYou01 <yashugarg012003@gmail.com>
2025-12-24 15:06:01 +0530

    UC# created

--- Program.cs
+++ Program.cs
@@ -8,7 +8,18 @@
             int startP = 0;
             
            Random r = new Random();
-            int diceNumber = r.Next(1 - 7);
+            int diceNumber = r.Next(1 , 7);
+
+            int options = r.Next(0, 3);
+            if(options == 2)
+            {
+                startP+= diceNumber;
+            }
+            else if(options == 3)
+            {
+                startP-= diceNumber; 
+            }
+            
         }
     }
 }
