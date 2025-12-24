3fea26a5c20c736f2e746550a2f40f6e40ef5702
YashYou01 <yashugarg012003@gmail.com>
2025-12-24 15:46:04 +0530

    UC7  created

--- Program.cs
+++ Program.cs
@@ -1,52 +1,56 @@
-﻿namespace SnakesAndLadder
+﻿using System;
+
+namespace SnakesAndLadder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int player;
-            int currentP = 0;
-            int diceCount = 0;
+            int[] position = { 0, 0 };
+            int currentPlayer = 0;
+            Random random = new Random();
 
+           
             
-           Random r = new Random();
-            while (currentP != 100)
-            {
-
-                int diceNumber = r.Next(1, 7);
-                diceCount++;
-
-                int option = r.Next(0, 3);
-                int prevP = currentP;
-                if (option == 0)
+                while (position[currentPlayer]!=100)
                 {
-                    Console.WriteLine("Option: No Play");
-                }
+                    int die = random.Next(1, 7);
+                    int option = random.Next(0, 3); 
 
-                else if (option == 1)
-                {
-                    currentP += diceNumber;
-                }
-                else
-                {
-                    currentP -= diceNumber;
-                }
-                if (currentP < 0)
-                {
-                    currentP = 0;
-                }
-                if (currentP > 100)
-                {
-                    currentP = prevP;
-                }
-                Console.WriteLine("Player current position: " + currentP);
-                Console.WriteLine("--------------------------------");
+                    Console.WriteLine("\nPlayer " + (currentPlayer + 1));
+                    Console.WriteLine("Die: " + die);
 
-            }
-                Console.WriteLine();
+                    if (option == 1) // Ladder
+                    {
+                        
+                            position[currentPlayer] += die;
 
-                Console.WriteLine("Player has won the game!");
+                        Console.WriteLine("Ladder! Position: " + position[currentPlayer]);
+                        
+                    }
+                    else if (option == 2) // Snake
+                    {
+                        position[currentPlayer] -= die;
+                        if (position[currentPlayer] < 0)
+                            position[currentPlayer] = 0;
 
+                        Console.WriteLine("Snake! Position: " + position[currentPlayer]);
+                        currentPlayer = 1 - currentPlayer; // Switch player
+                    }
+                    else // No Play
+                    {
+                        Console.WriteLine("No Play! Position: " + position[currentPlayer]);
+                        currentPlayer = 1 - currentPlayer; // Switch player
+                    }
+
+                    if (position[currentPlayer] == 100)
+                    {
+                        Console.WriteLine("\n Player " + (currentPlayer + 1) + " WON THE GAME!");
+                        break;
+                    }
+                }
         }
+
     }
-}
+ }
+
