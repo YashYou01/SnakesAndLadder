a657748917e1304d65088b672c34a9c72b178124
YashYou01 <yashugarg012003@gmail.com>
2025-12-24 14:42:48 +0530

    UC1 changed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int player;
+            int startP = 0;
+
         }
     }
 }
